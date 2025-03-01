900ddb05d628de84e8c8e6f213a587e24412d890
JakWai01 <salet@posteo.net>
2025-03-01 13:59:25 +0100

    feat: Add CLI options to show dag and fallback to qiskit

--- main.py
+++ main.py
@@ -14,38 +14,42 @@ from qiskit.transpiler.passes.layout import disjoint_utils
 
 @click.command()
 @click.option("-f", "--filename", type=str, required=True, help="Path to .qasm file")
-def main(filename: str):
+@click.option("-d", "--show-dag", type=bool, help="True if DAG should be shown")
+@click.option("-q", "--qiskit-fallback", type=bool, help="Use qiskit algorithm implementation")
+def main(filename: str, show_dag: bool, qiskit_fallback: bool):
     """Read in a .qasm file and print out a syntax tree."""
     circuit = QuantumCircuit.from_qasm_file(filename)
     circuit.draw('mpl') 
-    # plt.show()
     
-    dag = circuit_to_dag(circuit)
-    # image = dag_drawer(dag)
-    # image.show()
+    input_dag = circuit_to_dag(circuit)
+    
+    if show_dag:
+        input_dag_image = dag_drawer(input_dag)
+        input_dag_image.show()
     
     # A line with 10 physical qubits
     coupling_map = CouplingMap.from_line(10)
 
     # Generate initial 'trivial' mapping 
-    initial_mapping = generate_initial_mapping(dag)
+    initial_mapping = generate_initial_mapping(input_dag)
 
-    # Execute basic_swap
-    new_dag = basic_swap(dag, coupling_map, initial_mapping)
-    # bs = BasicSwap(coupling_map)
-    # new_dag = bs.run(dag)
+    # Execute basic swap algorithm
+    if qiskit_fallback:
+        bs = BasicSwap(coupling_map)
+        transpiled_dag = bs.run(input_dag)
+    else:
+        transpiled_dag = basic_swap(input_dag, coupling_map, initial_mapping)
     
-    # image = dag_drawer(res)
-    # image.show()
+    if show_dag:
+        output_dag_image = dag_drawer(transpiled_dag)
+        output_dag_image.show()
+    
+    # Convert DAG to circuit
+    transpiled_circuit = dag_to_circuit(transpiled_dag)
+    transpiled_circuit.draw('mpl')
 
-
-    # image = dag_drawer(new_dag)
-    # image.show()
-
-    res_circuit = dag_to_circuit(new_dag)
-    res_circuit.draw('mpl')
+    # Show circuits
     plt.show()
-
 
 def generate_initial_mapping(dag):
     canonical_register = dag.qregs["q"]
@@ -59,7 +63,7 @@ def basic_swap(dag, coupling_map, initial_mapping):
     new_dag = dag.copy_empty_like()
     
     if coupling_map is None:
-        raise TranspilerError("BasicSwap cannot run tieh coupling_map=None")
+        raise TranspilerError("BasicSwap cannot run with coupling_map=None")
 
     if len(dag.qregs) != 1 or dag.qregs.get("q", None) is None:
         raise TranspilerError("Basic swap runs on physical circuits only")
@@ -74,12 +78,13 @@ def basic_swap(dag, coupling_map, initial_mapping):
             physical_q0 = current_mapping[gate.qargs[0]]
             physical_q1 = current_mapping[gate.qargs[1]]
             
-            # With this example, we don't get in here
+            # Check if SWAP is required
             if coupling_map.distance(physical_q0, physical_q1) != 1:
                 # Insert a new layer with the SWAP(s)
                 swap_layer = DAGCircuit()
                 swap_layer.add_qreg(canonical_register)
-
+    
+                # Find shortest SWAP path
                 path = coupling_map.shortest_undirected_path(physical_q0, physical_q1)
 
                 for swap in range(len(path) - 2):
@@ -98,7 +103,7 @@ def basic_swap(dag, coupling_map, initial_mapping):
                 order = current_mapping.reorder_bits(new_dag.qubits)
                 new_dag.compose(swap_layer, qubits=order)
 
-                # Update current Layout
+                # Update current Mapping 
                 for swap in range(len(path) - 2):
                     current_mapping.swap(path[swap], path[swap + 1])
 
