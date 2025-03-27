ac04ed39f5c8b2a76da5381f72739c4924fa7c91
JakWai01 <salet@posteo.net>
2025-03-27 10:30:36 +0100

    feat: Initialize maturin project

---
+++ src/microscope/__init__.py
@@ -0,0 +1,6 @@
+from .microscope import *
+
+
+__doc__ = microscope.__doc__
+if hasattr(microscope, "__all__"):
+    __all__ = microscope.__all__
---
+++ src/tests/test_all.py
@@ -0,0 +1,6 @@
+import pytest
+import microscope
+
+
+def test_sum_as_string():
+    assert microscope.sum_as_string(1, 1) == "2"
